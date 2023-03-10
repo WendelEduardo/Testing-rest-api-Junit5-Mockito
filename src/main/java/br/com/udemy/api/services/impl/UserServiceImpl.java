6fae037b5d4c0cd8025a646314ce2e3a5bdd4a0d
Wendel Eduardo <wendeleduardo2002@gmail.com>
2023-03-10 16:06:07 -0300

    classe user service findById

---
+++ src/main/java/br/com/udemy/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package br.com.udemy.api.services.impl;
+
+import br.com.udemy.api.domain.Users;
+import br.com.udemy.api.repositories.UserRepository;
+import br.com.udemy.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public Users findById(Integer id) {
+        Optional<Users> usuario = repository.findById(id);
+        return usuario.orElse(null);
+    }
+
+}
