a605d29c258788fc4c0ef97bdd56965dad266ef8
Wendel Eduardo <wendeleduardo2002@gmail.com>
2023-03-10 15:21:10 -0300

    classe user

---
+++ src/main/java/br/com/udemy/api/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.udemy.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
