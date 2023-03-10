854eeeba13914041c7d0c48012b495a8f6d247c2
Wendel Eduardo <wendeleduardo2002@gmail.com>
2023-03-10 16:44:40 -0300

    tratamento de exceção para objeto não encontrado

--- src/main/java/br/com/udemy/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/udemy/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.udemy.api.services.impl;
 import br.com.udemy.api.domain.Users;
 import br.com.udemy.api.repositories.UserRepository;
 import br.com.udemy.api.services.UserService;
+import br.com.udemy.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public Users findById(Integer id) {
         Optional<Users> usuario = repository.findById(id);
-        return usuario.orElse(null);
+        return usuario.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 
 }
