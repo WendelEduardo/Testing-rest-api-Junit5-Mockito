b496b70cc32837af9202ae51d77433b43d136476
Wendel Eduardo <wendeleduardo2002@gmail.com>
2023-03-10 17:11:56 -0300

    classe user dto

---
+++ src/main/java/br/com/udemy/api/domain/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package br.com.udemy.api.domain.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+
+}
