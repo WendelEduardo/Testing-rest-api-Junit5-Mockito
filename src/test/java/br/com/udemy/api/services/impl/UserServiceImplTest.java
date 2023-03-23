7c0b0a7f7d2b83f02193b9c5bf0dfb178a08d098
Wendel Eduardo <wendeleduardo2002@gmail.com>
2023-03-23 16:54:31 -0300

    teste para classe user service metodo delete | Sucesso e Erro

--- src/test/java/br/com/udemy/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/udemy/api/services/impl/UserServiceImplTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -165,7 +165,30 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void whenDeleteThenReturnUserDeleted() {
+
+        // test repository
+        when(repository.findById(anyInt())).thenReturn(optionalUsers);
+        doNothing().when(repository).deleteById(anyInt());
+
+        // test service
+        service.delete(ID);
+
+        verify(repository, times(1)).deleteById(anyInt());
+
+    }
+
+
+    @Test
+    void whenDeleteThenReturnAnObjectNotFoundException(){
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+
+        try{
+            service.delete(ID);
+        }catch(Exception ex){
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
+        }
     }
 
     private void startUsers(){
